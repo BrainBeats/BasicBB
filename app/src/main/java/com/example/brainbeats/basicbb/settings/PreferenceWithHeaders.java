412c4969bb28853f67ee80adf99e51c6f01999b3
samutamm <samutamm@gmail.com>
2016-06-09 10:09:59 +0200

    Add settings to remove tags.

---
+++ app/src/main/java/com/example/brainbeats/basicbb/settings/PreferenceWithHeaders.java
@@ -0,0 +1,43 @@
+package com.example.brainbeats.basicbb.settings;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+
+import com.example.brainbeats.basicbb.R;
+
+import java.util.List;
+
+public class PreferenceWithHeaders extends PreferenceActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected boolean isValidFragment(String fragmentName) {
+        return PrefsFragment.class.getName().equals(fragmentName);
+    }
+
+    @Override
+    public void onBuildHeaders(List<Header> target) {
+        loadHeadersFromResource(R.xml.preference_headers, target);
+    }
+
+
+    public static class PrefsFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            // Make sure default values are applied.  In a real app, you would
+            // want this in a shared function that is used to retrieve the
+            // SharedPreferences wherever they are needed.
+            //PreferenceManager.setDefaultValues(getActivity(),
+            //        R.xml.advanced_preferences, false);
+
+            // Load the preferences from an XML resource
+            addPreferencesFromResource(R.xml.preferences);
+        }
+    }
+}
