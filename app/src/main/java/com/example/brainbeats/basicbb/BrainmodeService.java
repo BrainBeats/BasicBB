cff5b1fe2a0bd137d407571b5881ad06eb53deb4
samutamm <samutamm@gmail.com>
2016-06-15 11:24:28 +0200

    Send testmodel to server and get response.

--- app/src/main/java/com/example/brainbeats/basicbb/BrainmodeService.java
+++ app/src/main/java/com/example/brainbeats/basicbb/BrainmodeService.java
@@ -11,10 +11,12 @@ import android.os.Environment;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
+import android.support.annotation.NonNull;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.app.Activity;
+import android.widget.Toast;
 
 
 import com.emotiv.insight.IEdk;
@@ -39,27 +41,23 @@ public class BrainmodeService extends Service {
     private boolean isEnableWriteFile = false;
     private boolean isRunning = true;
     int userId;
+    public final String FILENAME = "bandpowerValue_Transient.csv";
 
     private BufferedWriter motion_writer;
     IEdk.IEE_DataChannel_t[] Channel_list = {IEdk.IEE_DataChannel_t.IED_AF3, IEdk.IEE_DataChannel_t.IED_T7, IEdk.IEE_DataChannel_t.IED_Pz,
             IEdk.IEE_DataChannel_t.IED_T8, IEdk.IEE_DataChannel_t.IED_AF4};
     String[] Name_Channel = {"AF3","T7","Pz","T8","AF4"};
-
-
+    private Context context;
 
     public void onCreate(){
-        //create the service
         super.onCreate();
+
+        context = getApplicationContext();
+
         Log.e("BrainMode service","started");
-
-
-
         final BluetoothManager bluetoothManager =
                 (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
         mBluetoothAdapter = bluetoothManager.getAdapter();
-
-
-
         //Connect to emoEngine
         IEdk.IEE_EngineConnect(this,"");
        Thread processingThread=new Thread()
@@ -67,23 +65,20 @@ public class BrainmodeService extends Service {
             @Override
             public void run() {
                 // TODO Auto-generated method stub
-                super.run();
-                while(isRunning)
-                {
-                    try
-                    {
-                        handler.sendEmptyMessage(0);
-                        handler.sendEmptyMessage(1);
-                        if(isEnablGetData && isEnableWriteFile)handler.sendEmptyMessage(2);
+            super.run();
+            while(isRunning) {
+                try {
+                    handler.sendEmptyMessage(0);
+                    handler.sendEmptyMessage(1);
+                    if(isEnablGetData && isEnableWriteFile)handler.sendEmptyMessage(2);
 
-                        Thread.sleep(100);
-                    }
-
-                    catch (Exception ex)
-                    {
-                        ex.printStackTrace();
-                    }
+                    Thread.sleep(200);
                 }
+
+                catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
             }
         };
         processingThread.start();
@@ -93,13 +88,13 @@ public class BrainmodeService extends Service {
         @Override
         public void handleMessage(Message msg) {
             switch (msg.what) {
-
                 case 0:
                     int state = IEdk.IEE_EngineGetNextEvent();
                     if (state == IEdkErrorCode.EDK_OK.ToInt()) {
                         int eventType = IEdk.IEE_EmoEngineEventGetType();
                         userId = IEdk.IEE_EmoEngineEventGetUserId();
                         if(eventType == IEdk.IEE_Event_t.IEE_UserAdded.ToInt()){
+                            Toast.makeText(context, "User added", Toast.LENGTH_SHORT).show();
                             Log.e("SDK","User added");
                             IEdk.IEE_FFTSetWindowingType(userId, IEdk.IEE_WindowsType_t.IEE_BLACKMAN);
                             isEnablGetData = true;
@@ -122,13 +117,12 @@ public class BrainmodeService extends Service {
                     else lock = false;
                     break;
                 case 2:
-                    for(int i=0; i < Channel_list.length; i++)
-                    {
+                    for(int i=0; i < Channel_list.length; i++) {
                         double[] data = IEdk.IEE_GetAverageBandPowers(Channel_list[i]);
                         if (data != null) {
                             if (data.length == 5) {
                                 try {
-                                    motion_writer.write(Name_Channel[i] + ",");
+                                    motion_writer.write(Name_Channel[i]);
                                     for (int j = 0; j < data.length; j++)
                                         addData(data[j]);
                                     motion_writer.newLine();
@@ -148,22 +142,33 @@ public class BrainmodeService extends Service {
     };
 
     public void addData(double data) {
-
         if (motion_writer == null) {
             return;
         }
-
         String input = "";
-        input += (String.valueOf(data) + ",");
+        input += ("," + String.valueOf(data));
         try {
             motion_writer.write(input);
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-
     }
+/*
+    public void addData(double data) {
+        if (motion_writer == null) {
+            return;
+        }
 
+        String input = "";
+        input += ("," + String.valueOf(data));
+        try {
+            motion_writer[MpNumber-1].write(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+*/
     public void stopBrainMode(){
         isRunning = false;
     }
@@ -175,19 +180,22 @@ public class BrainmodeService extends Service {
     private void setDataFile() {
         try {
             String eeg_header = "Channel , Theta ,Alpha ,Low beta ,High beta , Gamma ";
-            File root = Environment.getExternalStorageDirectory();
-            String file_path = root.getAbsolutePath()+ "/FFTSample/";
+            String file_path = getFilePath();
             File folder=new File(file_path);
-            if(!folder.exists())
-            {
+            if(!folder.exists()) {
                 folder.mkdirs();
             }
-            motion_writer = new BufferedWriter(new FileWriter(file_path+"bandpowerValue_Transient.csv"));
+            motion_writer = new BufferedWriter(new FileWriter(file_path+FILENAME));
             motion_writer.write(eeg_header);
             motion_writer.newLine();
         } catch (Exception e) {
             Log.e("","Exception"+ e.getMessage());
         }
+    }
+
+    public String getFilePath() {
+        File root = Environment.getExternalStorageDirectory();
+        return root.getAbsolutePath()+ "/brainbeats/";
     }
 
     private void StopWriteFile() {
@@ -200,7 +208,6 @@ public class BrainmodeService extends Service {
             e.printStackTrace();
         }
     }
-
 
     public void startWrite(){
         setDataFile();
@@ -223,16 +230,11 @@ public class BrainmodeService extends Service {
 
     @Override
     public boolean onUnbind(Intent intent){
-
         return false;
     }
-
 
     @Override
     public void onDestroy() {
         stopForeground(true);
     }
-
-
-
 }
